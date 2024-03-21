706e877a3d584d267fe050f53bbef3464f3c3a35
Hypocrite1023 <rex@rex.local>
2024-03-21 22:18:30 +0800

    init

---
+++ MoneyTracker/MoneyTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MoneyTracker
+//
+//  Created by 邱翊均 on 2024/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyTracker/MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by 邱翊均 on 2024/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
